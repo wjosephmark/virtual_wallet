ac690605298378c363935512449c57a9568d094b
wjosephmark <wjosephmark@gmail.com>
2020-03-12 10:42:20 -0600

    Initial Commit

--- virtual_wallet.py
+++ virtual_wallet.py
@@ -1,22 +1,89 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
 
-app = Flask(__name__)
+balance = 10000
+access = True
 
-joe = ("Joe", "Mark", -1,500)
+def mainMenu():
+    global balance
+    global access
 
-def greeting(first_name, last_name):
-    return f"Hello {first_name} {last_name}!"
+    def lockout_func():
+        global access
+        if access == False :
+            print(" ")
+            print("!!!!!!!!!!")
+            print(" ")
+            print("You're account has been frozen.  Please contact customer support for your unlock pin!")
+            print(" ")
+            print("!!!!!!!!!!")
+            print(" ")
+            lockout_input = int(input("Please enter unlock pin -> "))
+            if lockout_input == 3201:
+                    access = True
+                    print(" ")
+                    anykey=input("Pin accepted! Press enter to return to the menu")
+                    print(" ")
+                    mainMenu()
+                     
+            else:
+                print("That is not a valid input, please enter a valid unlock pin")
+                lockout_func()
+               
+    print(" ")
+    print("Hello, welcome to your virtual wallet!")
+    print(" ")
+    print("Please choose one of the following options:")
+    print(" ")
+    print("1. Check balance")
+    print(" ")
+    print("2. Withdraw")
+    print(" ")
+    print("3. Deposit")
+    print(" ")
+    print("4. Exit the program")
+    print(" ")
 
-def current_balance(balance):
-    return f"<h1> Your current balance is </h1> {balance}"
+    original_input = int(input("Please enter a number -> "))
 
-@app.route("/")
-def menu():
-    return "<h1> Welcome to your virtual wallet </h1>"
+    if original_input == 1203:
+        access = False
+        lockout_func()
 
-@app.route("/manage_account")
-def manage_account():
-    return greeting(joe), current_balance(joe)
+    elif original_input == 1:
+        print(" ")
+        print(f"You're current balance is ${balance}")
+        print(" ")
+        anykey=input("Press enter to return to the Main Menu")
+        mainMenu()
 
-if __name__ == "__main__":
-    app.run()
+    elif original_input == 2:
+        print(" ")
+        withdraw_amount = int(input("How much would you like to withdraw?  $"))
+        end_amount_withdraw = balance - withdraw_amount
+        balance = end_amount_withdraw
+        print(" ")
+        print(f"Withdrawl complete.  Your current balance is ${end_amount_withdraw}")
+        print(" ")
+        anykey=input("Press enter to return to the Main Menu")
+        mainMenu()
+        
+    elif original_input == 3:
+        print(" ")
+        deposit_amount = int(input("How much would you like to deposit?  $"))
+        end_amount_deposit = balance + deposit_amount
+        balance = end_amount_deposit
+        print(" ")
+        print(f"Deposit complete.  Your current balance is ${end_amount_deposit}")
+        print(" ")
+        anykey=input("Press enter to return to the Main Menu")
+        mainMenu()
+        
+    elif original_input == 4:
+        exit
+
+    else:
+        print(f"{original_input} is not a valid input.  Please enter a number between 1 - 4")
+        anykey=input("Press enter to return to the Main Menu")
+        mainMenu()
+
+mainMenu()
